98b37b34a1fec2f277fc441c0c18a9578972e77d
Ben Patch <fluffypatch@gmail.com>
2023-06-02 08:05:34 -0600

    initial commit

---
+++ mtg_card_identifier/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mtg_card_identifier
+//
+//  Created by Benjamin Patch on 6/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mtg_card_identifier/mtg_card_identifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtg_card_identifierApp.swift
+//  mtg_card_identifier
+//
+//  Created by Benjamin Patch on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct mtg_card_identifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

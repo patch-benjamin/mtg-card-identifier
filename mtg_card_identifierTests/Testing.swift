bc1d7e8ed31bb6f3185cb6398d1f35b8c04e9ac3
Ben Patch <fluffypatch@gmail.com>
2023-08-14 08:23:47 -0600

    commit blalblablabl

---
+++ mtg_card_identifierTests/Testing.swift
@@ -0,0 +1,16 @@
+//
+//  Testing.swift
+//  mtg_card_identifierTests
+//
+//  Created by Benjamin Patch on 8/14/23.
+//
+
+import XCTest
+import ComposableArchitecture
+
+@MainActor
+final class CounterFeatureTests: XCTestCase {
+  func testCounter() async {
+
+  }
+}

22c81dbd98c624787dd2d09070f7c8b6a8fdc87e
DylanMartin198 <111450540+DylanMartin198@users.noreply.github.com>
2023-09-12 11:56:18 -0600

    Added back DownloadDatabaseFilesView

---
+++ mtg_card_identifier/DownloadDatabaseFilesView.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadDatabaseFilesView.swift
+//  mtg_card_identifier
+//
+//  Created by Dylan Martin on 9/12/23.
+//
+
+import SwiftUI
+
+struct DownloadDatabaseFilesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadDatabaseFilesView_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadDatabaseFilesView()
+    }
+}

36226136827909fb4b4bab653a58f33dede445b2
Ben Patch <fluffypatch@gmail.com>
2024-08-01 09:24:28 -0600

    Added login Screen

---
+++ mtg_card_identifier/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  mtg_card_identifier
+//
+//  Created by Benjamin Patch on 8/1/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
